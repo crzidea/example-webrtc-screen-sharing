934401fc059c0de22cb2311f4adc82a7bd893a17
Crzidea <i@crzidea.com>
2023-06-02 19:05:25 +0800

    Fix top-level await

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,40 @@
 import "./style.css";
-import { getStreamFromCamera, getStreamFromDisplayMedia, playVideoWithStream } from "./media-devices";
+import {
+  getStreamFromCamera,
+  getStreamFromDisplayMedia,
+  playVideoWithStream,
+} from "./media-devices";
 import { startReceiving, startStreaming } from "./peer-connections";
 
-const matches = document.location.hash.match(/^\#\/(sender|receiver)\//);
-const role = matches ? matches[1] : "sender";
-if ("receiver" === role) {
-  // Get sender id and receiver id from hash
-  const matches = document.location.hash.match(/^\#\/receiver\/(.*)\/(.*)$/);
-  if (!matches) {
-    throw new Error("Receiver id and sender id not found in URL hash");
-  }
-  const roomId = matches[1];
-  const receiverId = matches[2];
-  startReceiving(roomId, receiverId, playVideoWithStream);
-} else {
-  // Get sender id from hash
-  const matches = document.location.hash.match(/^\#\/sender\/(.*)$/);
-  if (!matches) {
-    throw new Error("Sender id not found in URL hash");
-  }
-  const roomId = matches[1];
-  let stream
-  const MEDIA_STREAM = import.meta.env.VITE_MEDIA_STREAM;
-  if ('display' === MEDIA_STREAM) {
-    stream = await getStreamFromDisplayMedia();
+async function main() {
+  const matches = document.location.hash.match(/^\#\/(sender|receiver)\//);
+  const role = matches ? matches[1] : "sender";
+
+  if ("receiver" === role) {
+    // Get sender id and receiver id from hash
+    const matches = document.location.hash.match(/^\#\/receiver\/(.*)\/(.*)$/);
+    if (!matches) {
+      throw new Error("Receiver id and sender id not found in URL hash");
+    }
+    const roomId = matches[1];
+    const receiverId = matches[2];
+    startReceiving(roomId, receiverId, playVideoWithStream);
   } else {
-    stream = await getStreamFromCamera();
+    // Get sender id from hash
+    const matches = document.location.hash.match(/^\#\/sender\/(.*)$/);
+    if (!matches) {
+      throw new Error("Sender id not found in URL hash");
+    }
+    const roomId = matches[1];
+    let stream;
+    const MEDIA_STREAM = import.meta.env.VITE_MEDIA_STREAM;
+    if ("display" === MEDIA_STREAM) {
+      stream = await getStreamFromDisplayMedia();
+    } else {
+      stream = await getStreamFromCamera();
+    }
+    playVideoWithStream(stream);
+    startStreaming(roomId, stream);
   }
-  playVideoWithStream(stream);
-  startStreaming(roomId, stream);
 }
+main();
