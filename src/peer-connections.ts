072099874c29a21c70805d0383c6bcc43d2788da
Crzidea <i@crzidea.com>
2023-06-02 18:21:13 +0800

    Remove uuid dep

--- src/peer-connections.ts
+++ src/peer-connections.ts
@@ -1,5 +1,4 @@
 import { RealtimeChannel, createClient } from "@supabase/supabase-js";
-import { v4 as uuidv4 } from "uuid";
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
